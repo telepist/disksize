dcc45a26e4dc94e1d75dcce9a6b33727fafccaa2
Teemu <teemu.lepisto@gmail.com>
2025-11-15 15:06:45 +0200

    Remove kover, since it doesn't support kmp yet

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.jetbrains.compose)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kover)
 }
 
 kotlin {
